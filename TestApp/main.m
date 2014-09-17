812c1d9aba5bb3ebe7296e83716509751724aa4d
Franks, Kent <kfranks@cardinalsolutions.com>
2014-09-17 11:11:24 -0400

    Initial Commit

---
+++ TestApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestApp
+//
+//  Created by Kent Franks on 9/17/14.
+//  Copyright (c) 2014 Kef_Learning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
